e86261b108f60b9951cc5fa0f9f4f6d44743778c
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2025-09-07 19:07:40 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// let count;
+// console.log(count);
+// let newCount = 30;
+// console.log(newCount);
+// let tomato$Count;
+
+// const price = 30;
+// let appleCount = price * 7;
+// console.log(appleCount);
+
+// const userName = 'yoshipi';
+// let string = `Hello ${userName} !`;
+// console.log(string);
+
+// const a = '  \\ is\n singleQuate !';
+// console.log(a);
+
+// const b = `aaaaa
+// bbbbb`;
+// console.log(b);
+
+const userInput = '10.9';
+let calcResult;
+calcResult = Number(userInput) + 10;
+console.log(calcResult);
+calcResult = parseInt(userInput) + 10;
+console.log(calcResult);
+calcResult = parseFloat(userInput) + 10;
+console.log(calcResult);
+calcResult = +userInput + 10;
+console.log(calcResult);
+
+const tenNumber = 10;
+calcResult = String(tenNumber);
+console.log(calcResult);
+calcResult = tenNumber.toString();
+console.log(calcResult);
