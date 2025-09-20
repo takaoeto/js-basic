be2271ff5aa4c7f4d3b5fafa6bc954acf456fc82
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2025-09-20 13:56:33 +0900

    #68 カンマを使用することで１ループに複数の値を制御できる

--- main.js
+++ main.js
@@ -1,91 +1,97 @@
-let ok = true;
-let maybeOk = false;
+// let ok = true;
+// let maybeOk = false;
 
-//
-ok = 1 === 1;
-ok = 'hello' === 'hello';
-ok = 'hello' !== 'hello';
-//同値演算子(これは使わない)
-// ok = 1 == '1';
+// //
+// ok = 1 === 1;
+// ok = 'hello' === 'hello';
+// ok = 'hello' !== 'hello';
+// //同値演算子(これは使わない)
+// // ok = 1 == '1';
 
-const coffee1 = { name: 'Cafe Latte' };
-const coffee2 = { name: 'Cafe Latte' };
-const coffee3 = coffee1;
+// const coffee1 = { name: 'Cafe Latte' };
+// const coffee2 = { name: 'Cafe Latte' };
+// const coffee3 = coffee1;
 
-ok = coffee1 === coffee3;
-ok = coffee1.name === coffee2.name;
-const fruits1 = ['Apple', 'Banana'];
-const fruits2 = ['Apple', 'Banana'];
-ok = fruits1 === fruits2;
+// ok = coffee1 === coffee3;
+// ok = coffee1.name === coffee2.name;
+// const fruits1 = ['Apple', 'Banana'];
+// const fruits2 = ['Apple', 'Banana'];
+// ok = fruits1 === fruits2;
 
-ok = 1 >= 0;
-ok = 'a' > 'b';
-ok = 'A' < 'b';
-ok = 100;
-ok = 'false';
-ok = {};
+// ok = 1 >= 0;
+// ok = 'a' > 'b';
+// ok = 'A' < 'b';
+// ok = 100;
+// ok = 'false';
+// ok = {};
 
-if (ok) {
-  console.log(ok);
-  // console.log('OK!');
-} else {
-  console.log('NO!');
-}
-ok = false && false;
-ok = 'hi' && 'hi';
-console.log(ok);
+// if (ok) {
+//   console.log(ok);
+//   // console.log('OK!');
+// } else {
+//   console.log('NO!');
+// }
+// ok = false && false;
+// ok = 'hi' && 'hi';
+// console.log(ok);
 
-//セッション#58
-let userName = '' || 'User';
-console.log(userName);
-//??を使うことで、空文字として判定されるようになる
-userName = '' ?? 'User';
-console.log(userName);
-//『??』と論理演算子（|| &&）は同時に使うことができない。
-// userName = '' ?? 'User' && 1
+// //セッション#58
+// let userName = '' || 'User';
+// console.log(userName);
+// //??を使うことで、空文字として判定されるようになる
+// userName = '' ?? 'User';
+// console.log(userName);
+// //『??』と論理演算子（|| &&）は同時に使うことができない。
+// // userName = '' ?? 'User' && 1
 
-let okk;
-okk = okk ? 'OK!' : 'NG...';
-console.log(okk);
+// let okk;
+// okk = okk ? 'OK!' : 'NG...';
+// console.log(okk);
 
-//セッション#64
-function vegetableColor(vegetable) {
-  switch (vegetable) {
-    case 'tomato': {
-      const message = `${vegetable} is red`;
-      console.log(message);
-      break;
-    }
-    case 'carrot':
-    case 'pumpkin': {
-      const message = `${vegetable} is orange`;
-      console.log(message);
-      break;
-    }
-    case 'onion': {
-      const message = `${vegetable} is white`;
-      console.log(message);
-      break;
-    }
-    default: {
-      const message = `Not Found`;
-      console.log(message);
-      break;
-    }
-  }
-}
+// //セッション#64
+// function vegetableColor(vegetable) {
+//   switch (vegetable) {
+//     case 'tomato': {
+//       const message = `${vegetable} is red`;
+//       console.log(message);
+//       break;
+//     }
+//     case 'carrot':
+//     case 'pumpkin': {
+//       const message = `${vegetable} is orange`;
+//       console.log(message);
+//       break;
+//     }
+//     case 'onion': {
+//       const message = `${vegetable} is white`;
+//       console.log(message);
+//       break;
+//     }
+//     default: {
+//       const message = `Not Found`;
+//       console.log(message);
+//       break;
+//     }
+//   }
+// }
 
-vegetableColor('carrot');
+// vegetableColor('carrot');
 
 //セッション#66
-let number = 0;
-while (number < 10) {
-  number++;
-  console.log('while:' + +number);
-}
+// let number = 0;
+// while (number < 10) {
+//   number++;
+//   console.log('while:' + +number);
+// }
 
-tomatoCount = 0;
-do {
-  tomatoCount += 1;
-  console.log('do-while:' + tomatoCount);
-} while (tomatoCount < 1);
+// tomatoCount = 0;
+// do {
+//   tomatoCount += 1;
+//   console.log('do-while:' + tomatoCount);
+// } while (tomatoCount < 1);
+
+//セッション#68
+for (let pumpkinCount = 1, i = 1; pumpkinCount <= 10; pumpkinCount++, i += 1) {
+  console.log(`かぼちゃの数： ${pumpkinCount}`);
+  console.log(`iの値 ${i}`);
+}
